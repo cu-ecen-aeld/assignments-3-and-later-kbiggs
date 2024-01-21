02744e72b55d2fd5328ecc1db5808d2288803b0c
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-01-21 11:04:28 -0700

    Push progress

---
+++ finder-app/finder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filesdir=$1
+searchstr=$2
+#echo "filesdir: $filesdir"
+#echo "searchstr: $searchstr"
+
+# Return error if incorrect number of parameters specified
+if [ $# != 2 ]; then
+	echo "Incorrect number of parameters"
+	exit 1
+fi
+
+# Return error if filesdir isn't actually a directory
+if [ ! -d $filesdir ]; then
+	echo "$filesdir is not a directory"
+	exit 1
+fi
+
+# Find matching number of files and lines for searchstr
+# Echo out
+echo "The number of files are $(ls $filesdir -l | wc -l) and the number of matching lines are $(grep -r $searchstr $filesdir | wc -l)"
---
+++ finder-app/writer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+writefile=$1
+writestr=$2
+#echo "writefile: $1"
+#echo "writestr: $2"
+
+# Return error if incorrect number of parameters specified
+if [ $# != 2 ]; then
+	echo "Incorrect number of parameters"
+	exit 1
+fi
+
+# Create new file at writefile path with content of writestr
+echo $writestr > $writefile
+
+# Check if file was successfully created
+if [ ! -f $writefile ]; then
+	echo "$writefile was not created successfully"
+	exit 1
+fi
