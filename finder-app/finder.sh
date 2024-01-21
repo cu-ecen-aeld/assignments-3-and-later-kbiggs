0ac7a90371183f56077b6d3ac5e96a880a580473
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-01-21 12:02:26 -0700

    Update comments and sources

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# CU AESD Assignment 1
+# Katie Biggs
+# Jan 21, 2024
+
+# Store off arguments for easier readability
 filesdir=$1
 searchstr=$2
+
 #echo "filesdir: $filesdir"
 #echo "searchstr: $searchstr"
 
 # Return error if incorrect number of parameters specified
 if [ $# != 2 ]; then
-	echo "Incorrect number of parameters"
+	echo "Incorrect number of parameters. Expecting 2."
 	exit 1
 fi
 
@@ -17,6 +23,7 @@ if [ ! -d $filesdir ]; then
 	exit 1
 fi
 
-# Find matching number of files and lines for searchstr
-# Echo out
-echo "The number of files are $(ls $filesdir | wc -l) and the number of matching lines are $(grep -r $searchstr $filesdir | wc -l)"
+# Find number of files and number of matching lines for searchstr and echo out results to user
+# This code was based off of content from https://www.cyberciti.biz/faq/grep-count-lines-if-a-string-word-matches/. I used this content to help understand how to use grep to count the instances of a string in a file. I modified it to look recursively through directories.
+echo "The number of files are $(ls -r $filesdir | wc -l) and the number of matching lines are $(grep -r $searchstr $filesdir | wc -l)"
+
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+# CU AESD Assignment 1
+# Katie Biggs
+# Jan 21, 2024
+
+# Store off arguments for easier readability
 writefile=$1
 writestr=$2
+
 #echo "writefile: $1"
 #echo "writestr: $2"
 
 # Return error if incorrect number of parameters specified
 if [ $# != 2 ]; then
-	echo "Incorrect number of parameters"
+	echo "Incorrect number of parameters. Expecting 2."
 	exit 1
 fi
 
-# Create new file at writefile path with content of writestr
+# If writefile doesn't exist, create new file at writefile path
+# This code block was based off of content at https://stackoverflow.com/questions/125281/how-do-i-remove-the-file-suffix-and-path-portion-from-a-path-string-in-bash. I utilized this to identify how to separate the directory path from the full string provided.
 if [ ! -e $writefile ]; then
+	# Create parent directories if needed
 	mkdir -p $(dirname $writefile)
+	# Create file
 	touch $writefile
 fi
 
+# Write contents of string out to the desired file
+# Using > will overwrite the existing content
 echo $writestr > $writefile
 
 # Check if file was successfully created
@@ -24,3 +35,4 @@ if [ ! -f $writefile ]; then
 	echo "$writefile was not created successfully"
 	exit 1
 fi
+
