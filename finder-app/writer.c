0e9c2ba7095c9020bdc4cb2488d604e34beaad71
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-01-25 20:00:07 -0700

    Update comments

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,3 +1,7 @@
+/* CU AESD Assignment 2
+   Katie Biggs
+   January 25, 2025 */
+
 #include <stdio.h>
 #include <syslog.h>
 #include <sys/stat.h>
@@ -5,17 +9,19 @@
 #include <unistd.h>
 #include <string.h>
 
+/* Main function for writer application */
 int main(int argc, char* argv[])
 {
-	syslog(LOG_USER|LOG_DEBUG, "testing logging\n");
+	/* Set up connection to system loger with AESD prefix for readability */
+	/* Calling closelog() at end of program is optional */
+	const char * log_prefix = "AESD";
+	openlog(log_prefix, 0, LOG_USER);
 	
 	/* Check to make sure two arguments were specified */
 	if (argc != 3)
 	{
-		printf("Expected 2 arguments, not %d.\nArg 1 should be a filepath. Arg 2 should be the string to write.\n",
-			argc-1);
-		syslog(LOG_USER|LOG_ERR, "Expected 2 arguments, not %d.\nArg 1 should be a filepath. Arg 2 should be the string to write\n",
-			argc-1);
+		syslog(LOG_USER|LOG_ERR, "Expected 2 arguments, not %d. Arg 1 should be a file to write to. Arg 2 should be the string to write.",
+				argc-1);
 		return 1;
 	}
 
@@ -23,28 +29,32 @@ int main(int argc, char* argv[])
 	char * writefile = argv[1];
 	char * writestr = argv[2];
 
-	/* Create/open file to write to */
+	/* Create file to write to */
+	/* Creat will use O_CREAT|O_WRONLY|O_TRUNC */
+	/* User has rwx perms, others has r perms */
 	int fd = creat(writefile, S_IRWXU | S_IROTH);
 
-	/* Print error if file couldn't be created */
+	/* Log error if file couldn't be created */
 	if (fd == -1)
 	{
-		printf("%s could not be created successfully\n", writefile);
-		syslog(LOG_USER|LOG_ERR, "%s could not be created successfully\n", writefile);
+		syslog(LOG_USER|LOG_ERR, "%s could not be created successfully", writefile);
 		return 1;
 	}
 
 	/* Write string to file */
 	int write_bytes = write(fd, writestr, strlen(writestr));
+
+	/* Log error if the write operation failed */
 	if (write_bytes == -1)
 	{
-		printf("Error writing %s to %s\n", writestr, writefile);
-		syslog(LOG_USER|LOG_ERR, "Error writing %s to %s\n", writestr, writefile);
+		syslog(LOG_USER|LOG_ERR, "Error writing %s to %s", writestr, writefile);
 		return 1;
 	}
 	else
 	{
-		printf("Writing %s to %s\n", writestr, writefile);
-		syslog(LOG_USER|LOG_DEBUG, "Writing %s to %s\n", writestr, writefile);
+		syslog(LOG_USER|LOG_DEBUG, "Writing %s to %s", writestr, writefile);
 	}
+
+	/* Close file */
+	close(fd);
 }
