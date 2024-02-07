ffb5afbdd3189681ef2c29042d95f85d7a924ecd
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-02-06 19:17:49 -0700

    Save off threading progress

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -16,21 +16,28 @@ void* threadfunc(void* thread_param)
     struct thread_data* thread_func_args = (struct thread_data *) thread_param;
     int sec_to_wait_to_obtain = thread_func_args->wait_to_obtain_ms / 1000;
     int sec_to_wait_to_release = thread_func_args->wait_to_release_ms / 1000;
-    
+
     // sleep for wait to obtain ms
     sleep(sec_to_wait_to_obtain);
-
+    
     // obtain mutex
-    pthread_mutex_lock(thread_func_args->mutex);
+    if (pthread_mutex_lock(thread_func_args->mutex))
+    {
+        return thread_param;
+    }
+    
     // hold mutex for wait to release ms
     sleep(sec_to_wait_to_release);
-
-    // release mutex
-    pthread_mutex_unlock(thread_func_args->mutex);
 
     // if completed successfully, populate thread_complete_success with true
     thread_func_args->thread_complete_success = true;
     
+    // release mutex
+    if (pthread_mutex_unlock(thread_func_args->mutex))
+    {
+        return thread_param;
+    }
+
     return thread_param;
 }
 
@@ -51,13 +58,11 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
     thread_struct->wait_to_obtain_ms = wait_to_obtain_ms;
     thread_struct->wait_to_release_ms = wait_to_release_ms;
     thread_struct->mutex = mutex;
-
-    pthread_attr_t attr;
+    thread_struct->thread_complete_success = false;
+    pthread_mutex_init(thread_struct->mutex, NULL);
 
     // start threadfunc (nonblocking)
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-    int id = pthread_create(thread, &attr, threadfunc, thread_struct);
+    int id = pthread_create(thread, NULL, threadfunc, thread_struct);
     
     // error created thread, return false
     if (id != 0)
@@ -67,12 +72,16 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
     }
 
     // if thread completed successfully, put thread ID into thread param
-    /*if (thread_struct->thread_complete_success)
+    while (!thread_struct->thread_complete_success)
     {
-
-    }*/
+        //ERROR_LOG("waiting");
+    }
+    
+    pthread_join(*thread, NULL); 
+    ERROR_LOG("joined");
 
     // free thread_data with pointer returned from threadfunc
+    pthread_mutex_destroy(thread_struct->mutex);
     free(thread_struct);
     return true;
 }
