607c6d7acc6e2ac1ec5c6c9f66a1c973bd571ef9
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-02-14 20:30:40 -0700

    Code cleanup

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -35,21 +35,11 @@ static void signal_handler(int sig_num)
     }
 }
 
-// https://github.com/thlorenz/beejs-guide-to-network-samples/blob/master/lib/get_in_addr.c
-void *get_in_addr(struct sockaddr *sa) {
-  return sa->sa_family == AF_INET
-    ? (void *) &(((struct sockaddr_in*)sa)->sin_addr)
-    : (void *) &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
-
-int main(int argc, char* argv[])
+int register_signals(void)
 {
-    int    retval = 0, sock_fd = -1, client_fd;
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage client_addr;
-
-    // Register for signals
+    int retval = 0;
     struct sigaction new_action;
+
     memset(&new_action, 0, sizeof(struct sigaction));
     new_action.sa_handler = signal_handler;
     if (sigaction(SIGTERM, &new_action, NULL) != 0)
@@ -61,26 +51,43 @@ int main(int argc, char* argv[])
         retval = -1;
     }
 
-    // Open log
+    return retval;
+}
+
+// https://github.com/thlorenz/beejs-guide-to-network-samples/blob/master/lib/get_in_addr.c
+void *get_in_addr(struct sockaddr *sa)
+{
+  return sa->sa_family == AF_INET
+    ? (void *) &(((struct sockaddr_in*)sa)->sin_addr)
+    : (void *) &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char* argv[])
+{
+    int    retval = 0, sock_fd = -1, client_fd;
+    FILE   *fp;
+    struct addrinfo hints, *serv_info, *p;
+    struct sockaddr_storage client_addr;
+
+    // Register for signals
+    retval = register_signals();
+
+    // Open syslog
     openlog("AESD Socket", 0, LOG_USER);
 
-    // Open file for writing
-    //int fd = open(LOG_FILE, O_CREAT|O_APPEND|O_WRONLY|O_DSYNC, S_IRWXU | S_IROTH);
-    FILE *fp;
-
-    // open stream socket with port 9000 (fail and return -1 if any socket connection steps fail)
+    // open stream socket with port 9000
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if (getaddrinfo(NULL, PORT, &hints, &servinfo) != 0)
+    if (getaddrinfo(NULL, PORT, &hints, &serv_info) != 0)
     {
         printf("Error getting address info\n");
         retval = -1;
     }
 
-    for(p = servinfo; p != NULL; p = p->ai_next)
+    for (p = serv_info; p != NULL; p = p->ai_next)
     {
         sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
         if (sock_fd == -1)
@@ -93,6 +100,7 @@ int main(int argc, char* argv[])
         int yes = 1;
         if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) != 0)
         {
+            printf("Error setting socket options\n");
             retval = -1;
             break;
         }
@@ -105,21 +113,19 @@ int main(int argc, char* argv[])
         }
     }
 
-    freeaddrinfo(servinfo);
+    freeaddrinfo(serv_info);
 
     // listen for connection
     if (listen(sock_fd, 5) != 0)
     {
+        printf("Error listening\n");
         retval = -1;
     }
     
     // Accept connections until SIGINT or SIGTERM received
     while (!sigint_found && !sigterm_found && (retval != -1))
     {
-        printf("in while\n");        
-
         // accept connection
-        // log message to syslog when client connects "Accepted connection from XXXX" <-x is IP address
         socklen_t client_addr_size = sizeof(client_addr);
         client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_addr_size);
         if (client_fd == -1)
@@ -128,6 +134,7 @@ int main(int argc, char* argv[])
             continue;
         }
 
+        // log message to syslog when client connects "Accepted connection from XXXX" <-x is IP address
         char s[INET6_ADDRSTRLEN];
         inet_ntop(client_addr.ss_family,
                   get_in_addr((struct sockaddr *)&client_addr),
@@ -136,34 +143,32 @@ int main(int argc, char* argv[])
         printf("Accepted connection from %s\n", s);
         syslog(LOG_USER, "Accepted connection from %s", s);
 
-        // Receive data over connection
-        // Create /var/tmp/aesdsocketdata & append data received
-        // Each packet is complete when newline character is received
-        // Packet should be shorter than available heap, but handle malloc failures to discard over-length packets
         char buf[256] = {0};
-        int bytes_recv;
+        int  bytes_recv;
         bool more_bytes_to_read = true;
+        // Create /var/tmp/aesdsocketdata & append data received
         fp = fopen(LOG_FILE, "a+");
-        do
+        while (more_bytes_to_read)
         {
+            // Receive data over connection
             bytes_recv = recv(client_fd, buf, 256, 0);
-            if (bytes_recv <= 0)
-            {
-                more_bytes_to_read = false;
-            }
-            
             if (bytes_recv == -1)
             {
                 printf("Error receiving\n");
+                more_bytes_to_read = false;
                 retval = -1;
                 continue;
+            }
+            else if (bytes_recv == 0)
+            {
+                more_bytes_to_read = false;
             }
             
             printf("bytes recv %d\n", bytes_recv);
             printf("bytes %s\n", buf);
 
+            // Each packet is complete when newline character is received
             char * new_line_found = memchr(buf, '\n', 256);
-
             if (new_line_found != NULL)
             {
                 more_bytes_to_read = false;
@@ -171,15 +176,15 @@ int main(int argc, char* argv[])
 
             fwrite(buf, bytes_recv, 1, fp);
             printf("file write complete\n");
-        } while (more_bytes_to_read);
+        }
 
         fclose(fp);
         printf("file closed for write\n");
 
         // Once received data packet completes, return full content of /var/tmp/aesdsocketdata to client
-        fp = fopen(LOG_FILE, "r+");
         char read_buf[256] = {0};
-        int bytes_read;
+        int  bytes_read;
+        fp = fopen(LOG_FILE, "r+");
         printf("file opened for read\n");
         while (!feof(fp))
         {
@@ -190,7 +195,7 @@ int main(int argc, char* argv[])
             int bytes_sent = send(client_fd, msg_to_send, bytes_read, 0);
             printf("bytes sent %d\n", bytes_sent);
         }
-        fclose(fp);      
+        fclose(fp);
 
         // Log message to syslog when connection closes "Closed connection of XXX"
         close(client_fd);
@@ -198,9 +203,9 @@ int main(int argc, char* argv[])
         printf("closed\n");
     }
     
-    // When SIGINT or SIGTERM received, complete open connection operations, close open sockets, delete /var/tmp/aesdsocketdata
-    // Also log message to syslog "Caught signal, exiting"
-    
+    // When SIGINT or SIGTERM received,
+    // complete open connection operations, close open sockets, delete /var/tmp/aesdsocketdata
+    // Log message to syslog "Caught signal, exiting"    
     printf("got signal\n");
     syslog(LOG_USER, "Caught signal, exiting");
     closelog();
