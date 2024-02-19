fa29a12c5385bb92a41e500f5c290fa5a2b9e29b
katie <katie@Ubuntu.myguest.virtualbox.org>
2024-02-18 19:25:19 -0700

    Add error handling, remove prints, logic cleanup

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,14 +2,18 @@
 
 mode=$1
 
-# Start aesdsocket in daemon mode
-if [ "$mode" = "start" ]; then
-    echo Starting aesdsocket as daemon
-    start-stop-daemon --start -n aesdsocket --startas /usr/bin/aesdsocket -- -d
-# Stop aesdsocket and send SIGTERM
-elif [ "$mode" = "stop" ]; then
-    echo Stopping aesdsocket
-    start-stop-daemon --stop -n aesdsocket
-else
-    echo Need to specify either start or stop
-fi
+case "$1" in
+    # Start aesdsocket in daemon mode
+    start)
+        echo "Starting aesdsocket as daemon"
+        start-stop-daemon --start -n aesdsocket --startas /usr/bin/aesdsocket -- -d
+        ;;
+    # Stop aesdsocket and send SIGTERM
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon --stop -n aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,6 +1,6 @@
 /* CU AESD Assignment 5
    Katie Biggs
-   February 10, 2024    */
+   February 18, 2024   */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,19 +20,16 @@
 const char * LOG_FILE = "/var/tmp/aesdsocketdata";
 const char * PORT = "9000";
 
-bool sigint_found = false;
-bool sigterm_found = false;
+int sock_fd = -1;
 
 static void signal_handler(int sig_num)
 {
-    if (sig_num == SIGINT)
-    {
-        sigint_found = true;
-    }
-    else if (sig_num == SIGTERM)
-    {
-        sigterm_found = true;
-    }
+    syslog(LOG_USER, "Caught signal, exiting");
+    closelog();
+    close(sock_fd);
+    shutdown(sock_fd, SHUT_RDWR);
+    remove(LOG_FILE);
+    exit(EXIT_SUCCESS);
 }
 
 int register_signals(void)
@@ -54,6 +51,7 @@ int register_signals(void)
     return retval;
 }
 
+// Below function to get address info was utilized from the BGNet guide
 // https://github.com/thlorenz/beejs-guide-to-network-samples/blob/master/lib/get_in_addr.c
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -64,7 +62,7 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main(int argc, char* argv[])
 {
-    int    retval = 0, sock_fd = -1, client_fd;
+    int    retval = 0, client_fd;
     FILE   *fp;
     struct addrinfo hints, *serv_info, *p;
     struct sockaddr_storage client_addr;
@@ -74,7 +72,6 @@ int main(int argc, char* argv[])
     if (argc > 1 && (strcmp(argv[1], "-d") == 0))
     {
         run_daemon = true;
-        printf("a daemon\n");
     }
 
     // Register for signals
@@ -91,7 +88,7 @@ int main(int argc, char* argv[])
 
     if (getaddrinfo(NULL, PORT, &hints, &serv_info) != 0)
     {
-        printf("Error getting address info\n");
+        syslog(LOG_ERR, "Error getting address info");
         retval = -1;
     }
 
@@ -100,7 +97,7 @@ int main(int argc, char* argv[])
         sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
         if (sock_fd == -1)
         {
-            printf("Error getting socket file descriptor\n");
+            syslog(LOG_ERR, "Error getting socket file descriptor");
             retval = -1;
             break;
         }
@@ -108,14 +105,14 @@ int main(int argc, char* argv[])
         int yes = 1;
         if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) != 0)
         {
-            printf("Error setting socket options\n");
+            syslog(LOG_ERR, "Error setting socket options");
             retval = -1;
             break;
         }
 
         if (bind(sock_fd, p->ai_addr, p->ai_addrlen) != 0)
         {
-            printf("Error binding\n");
+            syslog(LOG_ERR, "Error binding");
             retval = -1;
             break;
         }
@@ -123,53 +120,77 @@ int main(int argc, char* argv[])
 
     freeaddrinfo(serv_info);
 
-    // Check for daemon
+    // Check for daemon, initializing if option was specified
     if (run_daemon)
     {
         pid_t pid = fork();
 
         if (pid < 0)
         {
+            close(sock_fd);
             exit(EXIT_FAILURE);
         }
         else if (pid > 0)
         {
             exit(EXIT_SUCCESS);
         }
+
+        // Create new session
+        if (setsid() == -1)
+        {
+            close(sock_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Change working directory
+        if (chdir("/"))
+        {
+            close(sock_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Close file descriptors
+        close(STDIN_FILENO);
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
+
+        // Redirect stdin/out/err to /dev/null
+        open("/dev/null", O_RDWR);
+        dup(0);
+        dup(0);
     }
 
     // listen for connection
     if (listen(sock_fd, 5) != 0)
     {
-        printf("Error listening\n");
+        syslog(LOG_ERR, "Error listening");
         retval = -1;
     }
     
     // Accept connections until SIGINT or SIGTERM received
-    while (!sigint_found && !sigterm_found && (retval != -1))
+    while (retval != -1)
     {
         // accept connection
         socklen_t client_addr_size = sizeof(client_addr);
         client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_addr_size);
         if (client_fd == -1)
         {
-            printf("Issue accepting connection\n");
+            syslog(LOG_ERR, "Issue accepting connection");
             continue;
         }
 
-        // log message to syslog when client connects "Accepted connection from XXXX" <-x is IP address
-        char s[INET6_ADDRSTRLEN];
+        // log message to syslog when client connects
+        char ip_addr[INET6_ADDRSTRLEN];
         inet_ntop(client_addr.ss_family,
                   get_in_addr((struct sockaddr *)&client_addr),
-                  s, sizeof(s));
+                  ip_addr, sizeof(ip_addr));
 
-        //printf("Accepted connection from %s\n", s);
-        syslog(LOG_USER, "Accepted connection from %s", s);
+        syslog(LOG_USER, "Accepted connection from %s", ip_addr);
 
+        // Create /var/tmp/aesdsocketdata & append data received
         char buf[256] = {0};
         int  bytes_recv;
         bool more_bytes_to_read = true;
-        // Create /var/tmp/aesdsocketdata & append data received
         fp = fopen(LOG_FILE, "a+");
         while (more_bytes_to_read)
         {
@@ -177,7 +198,7 @@ int main(int argc, char* argv[])
             bytes_recv = recv(client_fd, buf, 256, 0);
             if (bytes_recv == -1)
             {
-                printf("Error receiving\n");
+                syslog(LOG_ERR, "Error receiving data");
                 more_bytes_to_read = false;
                 retval = -1;
                 continue;
@@ -187,8 +208,7 @@ int main(int argc, char* argv[])
                 more_bytes_to_read = false;
             }
             
-            printf("bytes recv %d\n", bytes_recv);
-            printf("bytes %s\n", buf);
+            syslog(LOG_INFO, "Received %d bytes", bytes_recv);
 
             // Each packet is complete when newline character is received
             char * new_line_found = memchr(buf, '\n', 256);
@@ -198,42 +218,48 @@ int main(int argc, char* argv[])
             }
 
             fwrite(buf, bytes_recv, 1, fp);
-            printf("file write complete\n");
+            syslog(LOG_INFO, "Completed file write");
         }
 
         fclose(fp);
-        printf("file closed for write\n");
 
         // Once received data packet completes, return full content of /var/tmp/aesdsocketdata to client
         char read_buf[256] = {0};
         int  bytes_read;
         fp = fopen(LOG_FILE, "r+");
-        printf("file opened for read\n");
         while (!feof(fp))
         {
             bytes_read = fread(read_buf, 1, 256, fp);
-            printf("bytes read %d\n", bytes_read);
-            //printf("buffer %s\n", read_buf);
             char *msg_to_send = read_buf;
             int bytes_sent = send(client_fd, msg_to_send, bytes_read, 0);
-            printf("bytes sent %d\n", bytes_sent);
+            if (bytes_sent == -1)
+            {
+                syslog(LOG_ERR, "Error sending bytes");
+                retval = -1;
+            }
+            syslog(LOG_INFO, "Read %d bytes, sent %d bytes", bytes_read, bytes_sent);
         }
         fclose(fp);
 
-        // Log message to syslog when connection closes "Closed connection of XXX"
-        close(client_fd);
-        syslog(LOG_USER, "Closed connection from %s", s);
-        printf("closed\n");
+        // Log message to syslog when connection closes
+        if (close(client_fd) != 0)
+        {
+            syslog(LOG_ERR, "Error closing client socket %s", ip_addr);
+            retval = -1;
+        }
+        else
+        {
+            syslog(LOG_USER, "Closed connection from %s", ip_addr);
+        }        
     }
     
     // When SIGINT or SIGTERM received,
     // complete open connection operations, close open sockets, delete /var/tmp/aesdsocketdata
     // Log message to syslog "Caught signal, exiting"    
-    printf("got signal\n");
-    syslog(LOG_USER, "Caught signal, exiting");
+    syslog(LOG_INFO, "Closing aesdsocket application");
     closelog();
     close(sock_fd);
-    //fclose(fp);
+    shutdown(sock_fd, SHUT_RDWR);
     remove(LOG_FILE);
 
     return retval;
